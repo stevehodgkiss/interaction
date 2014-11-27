c39407e5a6cd0d7f4e1238255e10cd4259a0faaa
Steve Hodgkiss <steve@hodgkiss.me>
2014-11-27 13:17:23 +0700

    Document methods

--- lib/use_case.rb
+++ lib/use_case.rb
@@ -1,11 +1,24 @@
+require "active_support/all"
+require "active_model"
 require "use_case/version"
+require "use_case/validations"
+require "use_case/form"
 
 module UseCase
-  def self.included(base)
-    base.extend(ClassMethods)
-  end
+  extend ActiveSupport::Concern
+  include Validations
 
   module ClassMethods
+    # Executes the use case
+    #
+    # A use case object is instantiated with the supplied
+    # arguments, perform is called and the object is returned.
+    #
+    # @param args [*args] Arguments to initialize the use case with
+    #
+    # @return [Object] returns the use case object
+    #
+    # @since 0.0.1
     def perform(*args)
       use_case = new(*args)
       use_case.perform
@@ -13,23 +26,48 @@ module UseCase
     end
   end
 
+  # Executes use case logic
+  #
+  # Use cases must implement this method
+  #
+  # @since 0.0.1
   def perform
     raise NotImplementedError
   end
 
+  # Indicates if the use case was successful
+  #
+  # @return [TrueClass, FalseClass]
+  #
+  # @since 0.0.1
   def success?
     !failed?
   end
 
+  # Indicates whether the use case failed
+  #
+  # @return [TrueClass, FalseClass]
+  #
+  # @since 0.0.1
   def failed?
     @failed
   end
 
+  # Mark the use case as successful
+  #
+  # @return [TrueClass, FalseClass]
+  #
+  # @since 0.0.1
   def success
     @failed = false
     true
   end
 
+  # Mark the use case as failed
+  #
+  # @return [TrueClass, FalseClass]
+  #
+  # @since 0.0.1
   def failure
     @failed = true
     false
